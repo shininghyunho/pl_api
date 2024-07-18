20714dcd4a8c284a08a69eecd9e1f28bebf5421c
shininghyunho <gusgh3315@uos.ac.kr>
2024-07-18 15:21:33 +0900

    init

---
+++ src/main/kotlin/chaegang/pl_api/PlApiApplication.kt
@@ -0,0 +1,11 @@
+package chaegang.pl_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlApiApplication>(*args)
+}
