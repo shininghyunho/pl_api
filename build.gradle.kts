c64bf62f7fbede77169fa918706bbbcc27db846d
shininghyunho <gusgh3315@uos.ac.kr>
2024-08-13 15:54:12 +0900

    feat : add h2 db

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    // mysql connector
-    implementation("mysql:mysql-connector-java")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
