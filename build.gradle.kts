602caa2ec8ebb1b292af0981f4f1272ba9e849e8
shininghyunho <gusgh3315@uos.ac.kr>
2024-08-13 16:09:45 +0900

    chore : add mysql connector

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    // mysql connector
+    implementation("mysql:mysql-connector-java:8.0.33")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
