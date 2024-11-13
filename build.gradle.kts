0e86546b043f98df26579d7f48d9874c822524c5
shininghyunho <gusgh3315@uos.ac.kr>
2024-11-13 17:25:51 +0900

    feat : redis 환경설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,9 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     // gson
     implementation("com.google.code.gson:gson:2.10")
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    // kotest
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
